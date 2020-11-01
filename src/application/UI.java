4f7b99ef07e818ee062d230f037ca21a8b7f32a2
jairoy <jairo.yenecura@gmail.com>
2020-11-01 14:46:32 -0300

    Implementing possible moves of king

--- src/application/UI.java
+++ src/application/UI.java
@@ -62,6 +62,7 @@ public class UI {
 
 	}
 	
+	//OVERLOAD sobrecarregando o mesmo metodo.
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
